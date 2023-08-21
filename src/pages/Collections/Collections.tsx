a7e4a49f5d3fb18e2ff27e8e92493e3fa08a3762
Lucas <lucassanchezdl@gmail.com>
2023-08-21 01:40:26 -0300

    skeletons en collections

--- src/pages/Collections/Collections.tsx
+++ src/pages/Collections/Collections.tsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
-import { Container, Pagination, Box } from "@mui/material";
+import { Container, Pagination, Box, Skeleton, Alert } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import { ProductCard } from "../../components";
 import { useAsync, useFetchAndLoad } from "../../hooks";
 import { getProducts } from "../../services/products.service";
 import { createProductAdapter } from "../../adapters";
+
+const SkeletonLoader: React.FC = () => {
+  const skeletons = [1, 2, 3, 4];
+  return (
+    <Grid container marginBottom={3} spacing={2}>
+      {skeletons?.map((skeleton: any) => (
+        <Grid xs={12} sm={6} md={4} lg={3} key={skeleton}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Skeleton variant="rectangular" height={340} width={280} />
+          </Box>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 export const Collections: React.FC = () => {
   const [products, setProducts] = useState({} as any);
@@ -12,17 +27,19 @@ export const Collections: React.FC = () => {
   const [page, setPage] = useState(1);
   const [count, setCount] = useState(0);
 
-  const getApiData = async () => {
+  const getProductsData = async () => {
     const result = await callEndpoint(getProducts(page));
     return result;
   };
 
   const adaptProducts = (data: any) => {
-    const formattedProducts = data.data.products.map((product: any) =>
-      createProductAdapter(product)
-    );
-    setProducts(formattedProducts);
-    setCount(data.data.totalPages);
+    if (data) {
+      const formattedProducts = data.data.products.map((product: any) =>
+        createProductAdapter(product)
+      );
+      setProducts(formattedProducts);
+      setCount(data.data.totalPages);
+    }
   };
 
   const handleChangePage = (
@@ -32,7 +49,7 @@ export const Collections: React.FC = () => {
     setPage(value);
   };
 
-  useAsync(getApiData, adaptProducts, () => {}, [page]);
+  useAsync(getProductsData, adaptProducts, () => {}, [page]);
 
   return (
     <Container maxWidth="lg">
@@ -46,26 +63,31 @@ export const Collections: React.FC = () => {
         }}
       >
         {loading ? (
-          <h1>Cargando...</h1>
-        ) : (
-          <Grid container marginBottom={3} spacing={2}>
-            {products.length &&
-              products?.map((product: any) => (
+          <SkeletonLoader />
+        ) : products.length >= 1 ? (
+          <>
+            <Grid container marginBottom={3} spacing={2}>
+              {products?.map((product: any) => (
                 <Grid xs={12} sm={6} md={4} lg={3} key={product.id}>
                   <Box sx={{ display: "flex", justifyContent: "center" }}>
                     <ProductCard {...product} />
                   </Box>
                 </Grid>
               ))}
-          </Grid>
+            </Grid>
+            <Pagination
+              count={count}
+              page={page}
+              onChange={handleChangePage}
+              variant="outlined"
+              color="primary"
+            />
+          </>
+        ) : (
+          <Alert variant="filled" severity="info" sx={{ width: "100%" }}>
+            No hay productos por el momento
+          </Alert>
         )}
-        <Pagination
-          count={count}
-          page={page}
-          onChange={handleChangePage}
-          variant="outlined"
-          color="primary"
-        />
       </Box>
     </Container>
   );
